9621e4c8b9f4d9cfc5921840876f8ac72b959181
郝亚宁 <austinstd@163.com>
2023-08-18 11:05:58 +0800

    初始化项目

---
+++ src/main/java/com/example/window_current_limiting/WindowCurrentLimitingApplication.java
@@ -0,0 +1,13 @@
+package com.example.window_current_limiting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WindowCurrentLimitingApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(WindowCurrentLimitingApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/window_current_limiting/WindowCurrentLimitingApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.window_current_limiting;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WindowCurrentLimitingApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
